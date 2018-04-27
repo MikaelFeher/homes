98f5b9e21974ad7981246ccb7c595265c5083b1d
MikaelFeher <mikael.feher@gmail.com>
2018-04-27 22:30:29 +0200

    Initial Commit

---
+++ homes.js
@@ -0,0 +1,29 @@
+let homes = {
+    "hus1" : { 
+        "rooms": [{ 
+            "name": "Vardagsrum", 
+            "temperature": 21, 
+            "humidity": 0.5 
+        }, 
+        { 
+            "name": "farstun", 
+            "temperature": -3, 
+            "humidity": 95 
+        }] 
+    },
+    
+    "hus2" : { 
+        "rooms": [{ 
+            "name": "Vardagsrum", 
+            "temperature": 25, 
+            "humidity": 0.75 
+        }, 
+        { 
+            "name": "farstun", 
+            "temperature": 15, 
+            "humidity": 80 
+        }] 
+    }
+};
+
+module.exports = {"homes" : homes};
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const homes = require("./homes");
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Try /homes or /homes/[enter a house id here]/data");
+});
+
+app.get('/homes', (req, res) => {
+    res.send(homes);
+});
+
+app.get('/homes/:id/data', (req, res) => {
+    let id = req.params.id;
+    if(!homes.homes[id]) {
+        res.send("Data does not exist");
+    } else {
+        res.json(homes.homes[id]);
+    }
+});
+
+app.get("*", (req, res) => {
+    res.send("Oops, You tried accessing something that doesn't seem to exist...\n Try /homes or /homes/[houseId]/data");
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port: 3000");
+})
