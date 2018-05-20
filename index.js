22a6cb7b9d4436bf9b6dcbc75d6e1014fd350973
MikaelFeher <mikael.feher@gmail.com>
2018-05-20 11:26:22 +0200

    Updates the responses from the server.

--- index.js
+++ index.js
@@ -8,28 +8,32 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send("Try /homes or /homes/[enter a house id here]/data");
+    res.status(404).json({ error: "Try /homes or /homes/[enter a house id here]/data" });
 });
 
 app.get('/homes', (req, res) => {
-    res.send(homes);
+    res.status(200).json({ homes });
 });
 
 app.get('/homes/:id/data', (req, res) => {
     let id = req.params.id;
     let house = null;
-    
+
     house = homes.filter(home => home.id === id);
 
     if(!house.length) {
-        res.send("Data does not exist");
+        res.status(404).json({ error: "Data does not exist" });
     } else {
-        res.json(house);
+        res.status(200).json({ house : house[0] });
     }
 });
 
+app.get('/homes/whoami', (req, res) => {
+    res.sendStatus(418);
+})
+
 app.get("*", (req, res) => {
-    res.send("Oops, You tried accessing something that doesn't seem to exist...\n Try /homes or /homes/[houseId]/data");
+    res.status(404).json({ error: "Oops, You tried accessing something that doesn't seem to exist...\n Try /homes or /homes/[houseId]/data" });
 })
 
 app.listen(3000, () => {
